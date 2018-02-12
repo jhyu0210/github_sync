b47009697831cdcf174d9bfc876e51ea8e6f56ac
james yu <jhyu0210@gmail.com>
2018-02-12 11:09:19 +0900

    initial spring-starter project

---
+++ bbsApp/src/main/java/com/joyclad/bbs/BbsAppApplication.java
@@ -0,0 +1,12 @@
+package com.joyclad.bbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbsAppApplication.class, args);
+	}
+}
